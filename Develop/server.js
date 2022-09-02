d8f7f8993a5b4f918978daf2e055868e7d8adc31
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-09-01 22:54:45 -0400

    findById and validateNotes functions

--- Develop/server.js
+++ Develop/server.js
@@ -4,10 +4,28 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Use apiRoutes
+// use apiRoutes
 app.use("/api", apiroutes);
 app.use("/", htmlroutes);
+
+function findById(id, array) {
+  const result = array.filter((data) => data.id === id)[0];
+  return result;
+}
+
+// make sure user doesnt save empty notes
+function validateNotes(notes) {
+  if (!notes.title || typeof notes.title !== "string") {
+    return false;
+  }
+  if (!notes.text || typeof notes.text !== "string") {
+    return false;
+  }
+  return true;
+}
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+module.exports = validateNotes;
